f1af36bc9d81c3d70659b817a57a9e7259ce4b47
HimanshuSharma <56059924+himanshusharma1053@users.noreply.github.com>
2023-06-13 14:00:21 +0000

    test

--- src/test/java/com/develogical/QueryProcessorTest.java
+++ src/test/java/com/develogical/QueryProcessorTest.java
@@ -20,8 +20,5 @@ public class QueryProcessorTest {
     assertThat(queryProcessor.process("Shakespeare"), containsString("playwright"));
   }
 
-  @Test
-  public void chooseBiggestNumber() throws Exception {
-    assertThat(queryProcessor.process("which of the following numbers is the largest: 10, 55, 20?"), containsString("55"));
-  }
+  
 }
